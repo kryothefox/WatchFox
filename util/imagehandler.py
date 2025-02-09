85aa4ef1320c3c3f2a63d5f9d3db2f16c01181db
sillyfoxo <39372576+sillyfoxo@users.noreply.github.com>
2025-02-09 17:10:23 +0100

    clean up the image handler

--- util/imagehandler.py
+++ util/imagehandler.py
@@ -6,17 +6,6 @@ def overlayBubble(url):
     bubbleImage = Image.open("./assets/bubble.png").convert("RGBA")
     
     tempImage = Image.open(requests.get(url,stream=True).raw)
-
-    #Get size of bubble
-
-    widthBubble = bubbleImage.size[0]
-    heightBubble = bubbleImage.size[1]
-
-    #Get size of temp
-
-    widthTemp = tempImage.size[0]
-    heightTemp = tempImage.size[1]
-
     addedHeight = heightTemp+heightBubble
 
     bubbled  = Image.new("RGBA",(widthTemp,addedHeight))
