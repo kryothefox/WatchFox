7069b1f5d0ed1185969af3b50213d8cc039ebf35
kryothefox <39372576+kryothefox@users.noreply.github.com>
2025-03-05 18:05:25 +0100

    maybe one day we'll meet again

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ watchfox = discord.Bot(intents=botIntents)
 async def on_ready():
     import datetime
     log(f"Bot started on {str(datetime.date.today())} at {datetime.datetime.now().strftime('%H:%M %p')}")
-    #06/02/2025
+    #13/02/2024 - 06/02/2025
 
 
 watchfox.load_extension('cmds.utils')
